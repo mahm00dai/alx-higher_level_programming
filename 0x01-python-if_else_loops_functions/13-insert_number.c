6fcd47db878602e7f41dcd1697d94877d1e34a1a
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-09-10 18:48:53 -0700

    13. Insert in sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to the pointer to the head of the list
+ * @number: the number to insert
+ * Return: address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *current, *prev;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+
+	/* Case 1: Insert at the beginning */
+	if (*head == NULL || (*head)->n >= number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Case 2: Insert in the middle or at the end */
+	current = *head;
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+
+	/* Insert in between or at the end */
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
+}
+

59328bddf095c626ca9b849d63b9ff3d73133224
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-09-10 17:33:33 -0700

    . The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,9 @@ import random
 number = random.randint(-10000, 10000)  # This line should not change
 last_digit = number % 10  # Get the last digit of the number
 
-# Handle negative numbers
-if last_digit < 0:
-    last_digit = -last_digit
+# Adjust last_digit for negative numbers
+if number < 0:
+    last_digit = -abs(last_digit)
 
 # Determine the message based on the last digit
 if last_digit > 5:
@@ -14,4 +14,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit}\
+		    and is less than 6 and not 0")

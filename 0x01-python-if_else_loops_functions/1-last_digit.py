b3730d4234ff8ee926547f5294be7291db7627b8
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-09-10 17:41:25 -0700

    . The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_digit = number % 10  # Get the last digit of the number
 
 # Adjust last_digit for negative numbers
 if number < 0:
-    last_digit = -abs(last_digit)
+    last_digit -= 10
 
 # Determine the message based on the last digit
 if last_digit > 5:
@@ -15,4 +15,4 @@ elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
     print(f"Last digit of {number} is {last_digit}\
-		    and is less than 6 and not 0")
+ and is less than 6 and not 0")

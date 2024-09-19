eee1e7b479eecff263f86cf93ea98fb54f9253c9
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-09-18 19:07:49 -0700

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    
+    # Find the key with the maximum value
+    return max(a_dictionary, key=a_dictionary.get)

0b39f941b24641a1e6d280a0e97099263efe0796
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-11-03 02:06:40 -0800

    Answers

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class LockedClass:
+    """A class that prevents the creation of new instance attributes
+    except for 'first_name'."""
+
     __slots__ = ['first_name']
+
+    def __init__(self):
+        """Initialize a LockedClass instance without any attributes."""
+        pass

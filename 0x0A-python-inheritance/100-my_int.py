187f21ffc97181efb5bd730d453386234434aaae
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-11-05 01:32:05 -0800

    Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Module defining a rebellious integer class MyInt."""
 
+
 class MyInt(int):
     """Class that inverts the equality and inequality operators."""
-    
+
     def __eq__(self, other):
         """Invert the equality operator."""
         return super().__ne__(other)

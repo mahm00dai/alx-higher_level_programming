aebb6d8d744462a9535d95055518b1e28efbe1db
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-12-10 13:23:37 +0200

    avg_temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
 -- avg_temperatures
-SELECT city, 
-       ROUND(AVG((value * 9 / 5) + 32), 4) AS avg_temp
+SELECT city, AVG(value * 9/5 + 32) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;

bf8a83c76ef804319dc25b6d69936f53e5161684
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-12-10 12:35:46 +0200

    privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,12 @@
+-- 0-privileges.sql
+-- Create user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant privileges to user_0d_2 if necessary
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- List the privileges of user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List the privileges of user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

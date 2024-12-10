c3d04bec7d7c677261431a29323e906319059a08
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-12-10 13:02:08 +0200

    count_shows_by_genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- 13-count_shows_by_genre.sql
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+SELECT tg.name AS genre, COUNT(tsg.show_id) AS number_of_shows
+FROM tv_genres tg
+LEFT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+GROUP BY tg.name
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;

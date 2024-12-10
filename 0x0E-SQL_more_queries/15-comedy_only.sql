ad19411925fe0a1ae5b60ff0b4a6c9ae01d07aa5
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-12-10 13:04:09 +0200

    comedy_only

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- 15-comedy_only.sql
+SELECT ts.title
+FROM tv_shows ts
+JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+JOIN tv_genres tg ON tg.id = tsg.genre_id
+WHERE tg.name = 'Comedy'
+ORDER BY ts.title ASC;

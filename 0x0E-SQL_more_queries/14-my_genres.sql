1c322a18980ccfd89093d21f4e463ed7c05e8bca
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-12-10 13:03:24 +0200

    my_genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- 14-my_genres.sql
+SELECT tg.name
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_shows ts ON ts.id = tsg.show_id
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name ASC;

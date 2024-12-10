f923d5355a77eb510b57cfcccf21524927296d36
Mahmoud Abdelsalam <mahmoud.ai.abdelsalam@gmail.com>
2024-12-10 12:19:20 +0200

    move_to_utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,5 +8,5 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert the table first_table to UTF8 (utf8mb4, collation utf8mb4_unicode_ci)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the column 'name' in the first_table to UTF8 (utf8mb4, collation utf8mb4_unicode_ci)
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the column 'name' in the first_table to UTF8 (collation utf8mb4_unicode_ci)
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
